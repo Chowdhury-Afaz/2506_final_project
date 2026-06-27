1e4f8d9e1087f3ae9d2d1ed7925a1d1a27487055
abdur <abdur@gmail.com>
2026-06-27 19:45:39 +0600

    Backend Setup

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\FrontendController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'homepage'])->name('homepage');
 Route::get('/contact-us', [FrontendController::class,'contact'])->name('contact');
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
